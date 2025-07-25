2cca3afc47597344291f3aaef292bcafe4ad1b4f
Aishwarya-bagal <aishwarya@aarambhcorp.com>
2025-07-25 19:32:25 +0530

    Added partial database dump

---
+++ partial_database.sql
@@ -0,0 +1,197 @@
+-- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
+--
+-- Host: localhost    Database: disaster_relief
+-- ------------------------------------------------------
+-- Server version	8.0.41
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `userType` varchar(20) DEFAULT NULL,
+  `username` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `phone` varchar(50) DEFAULT NULL,
+  `location` varchar(100) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES ('volunteer','Aishwarya','bagalaishwarya2001@gmail.com',NULL,NULL,'$2a$10$NNKnYnx..QClChcfaOTzxOvg3seFwPJcencrSVlfFp53CNhGp/hG.',1,'2025-07-05 17:32:52'),('volunteer','xyzxyz','xyz@gmail.com',NULL,NULL,'$2a$10$dD8aVVHMAISHSuRgn1BtyOjfSUqYOeAHM7YfJLmDi9uAx3tUfDqE2',6,'2025-07-05 19:33:52'),('victim','gita','gita@gmail.com',NULL,NULL,'$2a$10$uMKnxmQmTnfmstQ6aoU9E.R5/YxDfLzjB3Rbc7tMaW87ILE1A5ZGe',12,'2025-07-05 21:19:06'),('volunteer','Anant','anant@gmail.com','Pune, Maharashtra','$2a$10$deSsG65Vb.kT4/n6.ITJdO1PFMm9cH4OAI8EAEpeLkPZP1k9FZNEi','$2a$10$Vef5zq4t6O6wsZyN9GlsGuhw64/s2lDQI6UuZxnX3El3fdDQ7Sl/C',13,'2025-07-06 16:16:23'),('victim','abcd','abcd@gmail.com','1234567890','pune','$2a$10$wVlxfFEHgVQsDxbuW9Itke2jMh.nCBzQim9vMcgnMGLK.8zykLFFm',16,'2025-07-06 19:11:29'),('admin','admin','admin@relief.com','9999999999','HQ','$2a$10$qKCGnGcQ5QfNquKPIqtGyOIzpx0HfURe2x7MvYyMz9fRzDxwBvXRS',17,'2025-07-06 22:26:07'),('volunteer','sapna','sapna@gmail.com','4567896754','kolhapur','$2a$10$nBIpf36YThswpkICtkVAS.QDCM7KUEU0AWSMNYo3wI4hummFcMRJ6',18,'2025-07-07 00:49:12'),('admin','disaster','disaster@gmail.com',NULL,NULL,'$2a$12$XkIO6E7N8YjYVdCR61DRpOwOtYcaUWo4XYb3NIa7ipS1doRnbwdr2',19,'2025-07-07 01:26:04');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tasks`
+--
+
+DROP TABLE IF EXISTS `tasks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tasks` (
+  `task_id` int NOT NULL AUTO_INCREMENT,
+  `request_id` int DEFAULT NULL,
+  `volunteer_id` int DEFAULT NULL,
+  `assigned_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `status` varchar(50) DEFAULT 'Assigned',
+  PRIMARY KEY (`task_id`),
+  KEY `request_id` (`request_id`),
+  KEY `volunteer_id` (`volunteer_id`),
+  CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`request_id`) REFERENCES `requests` (`id`),
+  CONSTRAINT `tasks_ibfk_2` FOREIGN KEY (`volunteer_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tasks`
+--
+
+LOCK TABLES `tasks` WRITE;
+/*!40000 ALTER TABLE `tasks` DISABLE KEYS */;
+INSERT INTO `tasks` VALUES (1,1,1,'2025-07-16 13:24:22','Completed'),(2,2,1,'2025-07-16 13:24:27','Completed'),(3,3,1,'2025-07-16 13:24:29','Assigned'),(4,1,1,'2025-07-17 15:44:00','Completed'),(5,1,13,'2025-07-17 18:51:24','Assigned'),(6,1,13,'2025-07-17 18:51:30','Assigned'),(7,1,13,'2025-07-23 11:07:57','Assigned');
+/*!40000 ALTER TABLE `tasks` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `requests`
+--
+
+DROP TABLE IF EXISTS `requests`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `requests` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) DEFAULT NULL,
+  `help_type` varchar(50) DEFAULT NULL,
+  `description` text,
+  `status` varchar(20) DEFAULT 'Pending',
+  `date_requested` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `requests`
+--
+
+LOCK TABLES `requests` WRITE;
+/*!40000 ALTER TABLE `requests` DISABLE KEYS */;
+INSERT INTO `requests` VALUES (1,'gita','Food','i want the food','Assigned','2025-07-05 15:50:20'),(2,'abcd','Food','i want the food for the people who are need help ','Assigned','2025-07-06 13:42:40'),(3,'gita','Medical','urgent needed','Assigned','2025-07-06 19:22:58');
+/*!40000 ALTER TABLE `requests` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `inventory`
+--
+
+DROP TABLE IF EXISTS `inventory`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `inventory` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `item_name` varchar(100) DEFAULT NULL,
+  `quantity` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `item_name` (`item_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `inventory`
+--
+
+LOCK TABLES `inventory` WRITE;
+/*!40000 ALTER TABLE `inventory` DISABLE KEYS */;
+INSERT INTO `inventory` VALUES (1,'medecine',35),(2,'food',50),(3,'water bottel',50);
+/*!40000 ALTER TABLE `inventory` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `centers`
+--
+
+DROP TABLE IF EXISTS `centers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `centers` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `location` varchar(100) DEFAULT NULL,
+  `capacity` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `centers`
+--
+
+LOCK TABLES `centers` WRITE;
+/*!40000 ALTER TABLE `centers` DISABLE KEYS */;
+INSERT INTO `centers` VALUES (1,'pune relief center','Pune, Maharashtra',5),(2,'Help Recovery Rehabilitation Home','pune',50),(3,'Santulan','mumbai',30);
+/*!40000 ALTER TABLE `centers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `contact_messages`
+--
+
+DROP TABLE IF EXISTS `contact_messages`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `contact_messages` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `message` text NOT NULL,
+  `submitted_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `contact_messages`
+--
+
+LOCK TABLES `contact_messages` WRITE;
+/*!40000 ALTER TABLE `contact_messages` DISABLE KEYS */;
+INSERT INTO `contact_messages` VALUES (1,'Aishwarya','bagalaishwarya2001@gmail.com','hello how can i participate in this management system','2025-07-23 17:34:39');
+/*!40000 ALTER TABLE `contact_messages` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-07-25 19:27:27
